81a55025e1565a21a3b05cc28b5335a02b16d922
unknown <ola.ses@yahoo.com>
2022-09-03 11:42:19 +0100

    to add arrows to decrease and allow for mobile

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 
+import React from 'react'
 import Sidebar from "../../components/sidebar/Sidebar"
 import Header from "../../components/header/Header"
 import "./home.scss"
@@ -17,12 +18,12 @@ const Home = () => {
 <div className="top-part">
     <div className="left-part">
       <div className="two-left-widgets">
-        <Smallwidgets className="widget-style"/>
-        <Smallwidgets className="widget-style"/>
+        <Smallwidgets header="Customers" price="4,000" className="widget-style" changes="increase" percentage="+2.5%"/>
+        <Smallwidgets  header="Conversions" className="widget-style" price="6,000" changes="decrease" percentage="-2.0%"/>
       </div>
       <div className="right-left-widgets">
-        <Smallwidgets className="widget-style"/>
-        <Smallwidgets className="widget-style"/>
+        <Smallwidgets header="Orders" className="widget-style" price="2,000" changes="decrease" percentage="-3.5%"/>
+        <Smallwidgets header="Earnings" className="widget-style" price="4,000" changes="increase" percentage="+2.2%"/>
       </div>
 
 
